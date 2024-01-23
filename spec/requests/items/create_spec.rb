fffa3b262d352f9e97b478af75f7fbb982567ba6
Kyle Rego <regoky@outlook.com>
2024-01-23 03:45:50 -0500

    Add first test

---
+++ spec/requests/items/create_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "POST /items", "#create" do
+  subject(:post_create) do
+    post items_path, params:
+  end
+
+  let(:params) do
+    { item: { name:, description:, notes:, item_id:, container:, weight:, location_id: } }
+  end
+
+  let(:name) { "name of item" }
+  let(:description) { "description of item" }
+  let(:notes) { "some notes about the item" }
+  let(:item_id) { nil }
+  let(:container) { false }
+  let(:weight) { 5.5 }
+  let(:location_id) { nil }
+
+  it "creates an item" do
+    expect { post_create }.to change(Item, :count).by(1)
+  end
+end
