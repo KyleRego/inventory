7969e886c12373766be72f5191964e7e04fb18b8
Kyle Rego <regoky@outlook.com>
2024-01-25 06:32:45 -0500

    Add association to location

--- spec/models/item/associations_spec.rb
+++ spec/models/item/associations_spec.rb
@@ -3,11 +3,7 @@
 require "rails_helper"
 
 RSpec.describe Item do
-  context "associations" do
-    subject(:item) do
-      Item.new
-    end
+  subject(:item) { Item.new }
 
-    it { should belong_to(:item).class_name("Item").optional }
-  end
+  it { should belong_to(:item).class_name("Item").optional }
 end
---
+++ spec/models/location/associations_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Location do
+  subject { Location.new }
+
+  it { should have_many(:items) }
+end
