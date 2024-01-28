5ec10e1d1b9ba1777a426a2ee20634af67e09ea6
Kyle Rego <regoky@outlook.com>
2024-01-28 11:39:10 -0500

    Finish initial hamburger menu

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -1,17 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "menu" ];
+  static targets = [ "menu", "showButton", "hideButton" ];
 
   connect() {
+    this.showButtonTarget.addEventListener("click", () => this.show());
+    this.hideButtonTarget.addEventListener("click", () => this.hide());
     console.log("hamburger controller has been connected");
   }
 
   show() {
-    this.menuTarget.hidden = false;
+    this.menuTarget.classList.remove("invisible");
+    this.showButtonTarget.classList.add("invisible");
   }
 
   hide() {
-    this.menuTarget.hidden = true;
+    this.menuTarget.classList.add("invisible");
+    this.showButtonTarget.classList.remove("invisible");
   }
 }
