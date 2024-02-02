4baf1e1307880b3466e77781246ed8caf7e8d901
Kyle Rego <regoky@outlook.com>
2024-02-02 05:20:30 -0500

    Refactor, work on items show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: %i[ show edit update destroy ]
 
-  def index
-    @items = Item.all
-  end
-
   def show
     @location = @item.location
     @container_item = @item.item
@@ -26,36 +22,25 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
 
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to item_url(@item), notice: "Item was successfully created." }
-        format.json { render :show, status: :created, location: @item }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      redirect_to item_url(@item), notice: "Item was successfully created."
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to item_url(@item), notice: "Item was successfully updated." }
-        format.json { render :show, status: :ok, location: @item }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update(item_params)
+      redirect_to item_url(@item), notice: "Item was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @item.destroy
 
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: "Item was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to items_url, notice: "Item was successfully destroyed."
   end
 
   private
--- app/views/items/_item.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! item, :id, :name, :description, :notes, :item_id, :container, :location_id, :created_at, :updated_at
-json.url item_url(item, format: :json)
